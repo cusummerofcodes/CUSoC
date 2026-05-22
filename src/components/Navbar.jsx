2c851678ac3a14b386edd29156013e1c05309077
Pratik Kumar <pratik792584@gmail.com>
2026-05-22 16:35:39 +0530

    feat: update logo size, title and metadata

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,10 @@ const Navbar = () => {
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="flex items-center gap-3">
-              <img className="h-11 w-auto" src="/cusoc.png" alt="CUSoC Logo" />
+              <img className="h-14 w-auto" src="/cusoc.png" alt="CUSoC Logo" />
+              <span className="hidden lg:block text-gray-800 font-bold text-lg tracking-tight">
+                Chandigarh University Summer of Code
+              </span>
             </Link>
           </div>
 
